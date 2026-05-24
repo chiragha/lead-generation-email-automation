a75b0b102671640c8c917f702c875b0f7f471fd9
Shivani Sinha <147588548+chiragha@users.noreply.github.com>
2026-05-24 12:51:01 +0530

    Delete routes directory

--- routes/emailRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendMailController } from "../controllers/emailController.js";
-
-const router = express.Router();
-
-router.post("/send-email", sendMailController);
-
-export default router;
--- routes/leadRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { searchLeadController } from "../controllers/leadController.js";
-
-const router = express.Router();
-
-router.get("/search", searchLeadController);
-
-export default router;
